2fea6419814bd34865af79187586e0031c9b0288
Ben Porter <ben@kloudly.app>
2024-10-20 14:39:18 -0500

    Merged check-in-out UserList back into page.js since server actions are already separated into actions.js

--- app/check-in-out/page.js
+++ app/check-in-out/page.js
@@ -1,13 +1,54 @@
+'use client'
+
 import TitleAndBack from '../components/TitleAndBack'
-import UserList from './UserList'
 import Button from '../components/Button'
 import { getAllUsers } from './actions'
+import { useEffect, useState } from 'react'
+import FormRow from '../components/FormRow'
+import { startSession, endSession } from './actions'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/navigation'
 
-const CheckInOut = async ({ searchParams }) => {
+const CheckInOut = ({ searchParams }) => {
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [search, setSearch] = useState('')
+  const router = useRouter()
+
   const direction = searchParams.direction
 
-  const allUsers = await getAllUsers(direction)
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      const allUsers = await getAllUsers(direction)
+      setFilteredUsers(allUsers)
+    }
+    fetchAllUsers()
+  }, [direction])
 
+  const updateSearch = (e) => {
+    if (e?.preventDefault) e.preventDefault()
+    const searchValue = e.currentTarget.value
+    setSearch(searchValue)
+    setFilteredUsers(
+      allUsers?.filter((user) =>
+        user.fields.Name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    )
+  }
+  const handleClick = async (person) => {
+    try {
+      if (direction === 'in') {
+        await startSession(person.id)
+        toast.success('Check-in successful')
+      } else {
+        await endSession(person.id)
+        toast.success('Check-out successful')
+      }
+      router.push('/')
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+      console.error(error)
+    }
+  }
   return (
     <div className='w-3/4 mx-auto flex flex-col gap-8'>
       <TitleAndBack title={`check ${direction}`} />
@@ -16,7 +57,42 @@ const CheckInOut = async ({ searchParams }) => {
           <h4>First time? Register here</h4>
         </Button>
       )}
-      <UserList allUsers={allUsers} direction={direction} />
+      <form className='m-0 mb-4 h-full p-6 pb-8 grid grid-rows-[auto_auto_1fr] text-center overflow-hidden bg-[var(--accent)] rounded-lg shadow-md'>
+        <h4 className='text-2xl'>Select your name to check {direction}</h4>
+        <div className='mt-4 flex items-end gap-6 w-full justify-stretch'>
+          <FormRow
+            type='search'
+            name='search'
+            defaultValue={search}
+            placeholder='Search for your name'
+            onChange={updateSearch}
+            hideLabel
+            className='w-full mb-0 text-xl'
+            inputClassName='h-10 text-xl text-foreground bg-[var(--background)] hover:bg-[var(--background)]/80 p-4 rounded-lg w-full placeholder:text-[var(--foreground)] hover:shadow-md transition-shadow'
+          />
+          <Button
+            onClick={() => updateSearch({ currentTarget: { value: '' } })}
+            className='h-10 flex items-center btn form-btn delete-btn text-xl'
+            style='brown'
+          >
+            Clear
+          </Button>
+        </div>
+        <div className='mt-6 overflow-auto flex flex-col gap-4 min-h-[20rem] h-[calc(100vh-40rem)]'>
+          {filteredUsers?.map((person) => {
+            return (
+              <button
+                type='button'
+                className='m-0 mx-auto w-full py-4 px-8 bg-[var(--background)] shadow-md rounded-lg border-none text-xl hover:shadow-lg transition-shadow'
+                key={person.id}
+                onClick={() => handleClick(person)}
+              >
+                <h4>{person.fields.Name}</h4>
+              </button>
+            )
+          })}
+        </div>
+      </form>
     </div>
   )
 }
--- app/components/TitleAndBack.jsx
+++ app/components/TitleAndBack.jsx
@@ -2,7 +2,7 @@ import { IoMdArrowRoundBack } from 'react-icons/io'
 import Link from 'next/link'
 const TitleAndBack = ({ title, path }) => {
   return (
-    <div className='grid grid-cols-3 items-center capitalize text-3xl'>
+    <div className='grid grid-cols-[2rem_1fr_2rem] items-center capitalize text-3xl'>
       <div className='justify-self-start'>
         <Link href={path || '..'} className='link'>
           <IoMdArrowRoundBack />
